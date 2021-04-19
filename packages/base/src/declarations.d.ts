12c8d211f2770cbdf3bac4bb02f2c4833c6af405
Vladislav Navrocky <vladislav.navrocky@sebbia.org>
2021-04-19 18:47:31 +0300

    Запуск и livereload приложения с библиотеками

---
+++ packages/base/src/declarations.d.ts
@@ -0,0 +1,5 @@
+// this needed for import images in TS files
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
+declare module '*.json';
---
+++ packages/base/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import image from './test.png';
+
+export function HelloWorld() {
+  return <div><img src={image} width={100} />Hello World from Base Component</div>;
+}
